f4babfa5ec5b50883cffe2cbf23b015357e75923
Driver <314607329@qq.com>
2024-09-11 14:19:19 +0800

    Delete dist directory

--- dist/ArrayUtils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ArrayUtils = (function () {
-    function ArrayUtils() {
-    }
-    return ArrayUtils;
-}());
-exports.default = ArrayUtils;
--- dist/ClassUtils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ClassUtils = (function () {
-    function ClassUtils() {
-    }
-    return ClassUtils;
-}());
-exports.default = ClassUtils;
